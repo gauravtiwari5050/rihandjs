5ab62329a7921998f99d670e4858166f5c8f73c8
Gaurav Tiwari <gauravtiwari5050@gmail.com>
2017-10-01 13:44:54 +0530

    add constraints and route table

--- test/integration/routing/routes.js
+++ test/integration/routing/routes.js
@@ -3,14 +3,25 @@ const Rihand = require('../../../index.js');
 
 describe('integration: Routes', () => {
   describe('method: loadRoutes', () => {
-    Rihand.Routes.instance = null;
     it('should loadRoutes correctly', () => {
+      Rihand.Routes.instance = null;
       require('../../test_apps/basic_app/config/routes').loadRoutes();
       const rootNamespace = Rihand.Routes.getInstance();
       assert.equal(1, rootNamespace.namespaces.length);
       assert.equal(1, rootNamespace.routes.length);
       const apiNamespace = rootNamespace.namespaces[0];
       assert.equal('JSON', apiNamespace.props.format);
+    });
+  });
+
+  describe('method: getRouteTable', () => {
+    it('should return valid RouteTable instance', () => {
+      Rihand.Routes.instance = null;
+      require('../../test_apps/basic_app/config/routes').loadRoutes();
+      const rootNamespace = Rihand.Routes.getInstance();
+      const routeTable = rootNamespace.getRouteTable();
+      console.log("Route table entries");
+      console.log(routeTable.entries);
     });
   });
 });
--- test/test_apps/basic_app/config/routes.js
+++ test/test_apps/basic_app/config/routes.js
@@ -1,8 +1,14 @@
 const Rihand = require('../../../../index.js');
 
+const sampleConstraint = function (request, response, callback) {
+  callback(true);
+};
+const sampleConstraint2 = function (request, response, callback) {
+  callback(true);
+};
 module.exports.loadRoutes = () => {
-  Rihand.Routes.getInstance().setup(function () {
-    this.Get('/ => landing#home');
+  Rihand.Routes.getInstance().constraint(sampleConstraint).setup(function () {
+    this.Get('/ => landing#home').constraint(sampleConstraint2);
     this.namespace('api').json().setup(function () {
       this.namespace('v1').setup(function () {
         this.Get('/subscribers => subscribers#register').json();
