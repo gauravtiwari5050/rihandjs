48a95bbb14b5cf6c3a4f93f30e4d3d943000cecd
Gaurav Tiwari <gauravtiwari5050@gmail.com>
2017-10-02 16:15:07 +0530

    matches incoming route to correct target

--- test/integration/routing/routes.js
+++ test/integration/routing/routes.js
@@ -1,14 +1,17 @@
 const assert = require('assert');
 const Rihand = require('../../../index.js');
+const express = require('express');
+const request = require('supertest');
+const path = require('path');
 
-describe('integration: Routes', () => {
-  describe('method: loadRoutes', () => {
-    it('should loadRoutes correctly', () => {
-      Rihand.Routes.instance = null;
-      require('../../test_apps/basic_app/config/routes').loadRoutes();
-      const rootNamespace = Rihand.Routes.getInstance();
+describe('integration: Route', () => {
+  describe('method: loadRoute', () => {
+    it('should loadRoute correctly', () => {
+      Rihand.Route.instance = null;
+      require('../../test_apps/basic_app/config/routes').loadRoute();
+      const rootNamespace = Rihand.Route.getInstance();
       assert.equal(1, rootNamespace.namespaces.length);
-      assert.equal(1, rootNamespace.routes.length);
+      assert.equal(2, rootNamespace.routes.length);
       const apiNamespace = rootNamespace.namespaces[0];
       assert.equal('JSON', apiNamespace.props.format);
     });
@@ -16,12 +19,33 @@ describe('integration: Routes', () => {
 
   describe('method: getRouteTable', () => {
     it('should return valid RouteTable instance', () => {
-      Rihand.Routes.instance = null;
-      require('../../test_apps/basic_app/config/routes').loadRoutes();
-      const rootNamespace = Rihand.Routes.getInstance();
+      Rihand.Route.instance = null;
+      require('../../test_apps/basic_app/config/routes').loadRoute();
+      const rootNamespace = Rihand.Route.getInstance();
       const routeTable = rootNamespace.getRouteTable();
-      console.log("Route table entries");
-      console.log(routeTable.entries);
+      assert.equal(2, routeTable.entries.length);
+      assert.equal(2, routeTable.entries[0].targets.length);
+      assert.equal(1, routeTable.entries[1].targets.length);
+    });
+  });
+
+  describe('method: getRouter', () => {
+    it('should return valid Router instance', (done) => {
+      const rootNamespace = new Rihand.Route('/');
+      const applicationRoot = path.resolve(__dirname, '../../test_apps/basic_app');
+      rootNamespace.setApplicationRoot(applicationRoot);
+      /*
+        Set up the routes first
+       */
+      rootNamespace.setup(function () {
+        this.Get('/ => test_controller#root_valid');
+      });
+      const router = rootNamespace.getRouter();
+      const app = express();
+      app.use('/', router);
+      request(app)
+        .get('/')
+        .expect(200, done);
     });
   });
 });
--- test/test_apps/basic_app/config/routes.js
+++ test/test_apps/basic_app/config/routes.js
@@ -6,9 +6,10 @@ const sampleConstraint = function (request, response, callback) {
 const sampleConstraint2 = function (request, response, callback) {
   callback(true);
 };
-module.exports.loadRoutes = () => {
-  Rihand.Routes.getInstance().constraint(sampleConstraint).setup(function () {
+module.exports.loadRoute = () => {
+  Rihand.Route.getInstance().constraint(sampleConstraint).setup(function () {
     this.Get('/ => landing#home').constraint(sampleConstraint2);
+    this.Get('/ => landing#home2');
     this.namespace('api').json().setup(function () {
       this.namespace('v1').setup(function () {
         this.Get('/subscribers => subscribers#register').json();
