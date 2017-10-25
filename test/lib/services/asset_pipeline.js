b70b7230e7053a5ac0a1b32e27343a60c12b0422
Gaurav Tiwari <gauravtiwari5050@gmail.com>
2017-10-25 17:26:58 +0530

    add application method to initialize service by name

--- test/lib/services/asset_pipeline.js
+++ test/lib/services/asset_pipeline.js
@@ -1,9 +1,8 @@
 const assert = require('assert');
 const { expect } = require('chai'); // eslint-disable-line
-const { View } = require('../../../lib/views/view');
 const path = require('path');
 const { Application } = require('../../../lib/application/application');
-const { AssetPipelineService } = require('../../../lib/services/asset_pipeline_service');
+
 
 describe('class: AssetPipelineService', () => {
   describe('method: initialize', () => {
@@ -12,15 +11,14 @@ describe('class: AssetPipelineService', () => {
         rootDirectory: path.resolve(__dirname, '../../test_apps/basic_app/'),
         env: 'test',
       });
-      const assetPipelineService = new AssetPipelineService({
-        application,
-      });
-      assetPipelineService.initialize({}, (err) => {
+
+      application.initializeService('asset_pipeline', (err) => {
         if (err) {
           return done(err);
         }
+        const assetPipelineService = application.service('asset_pipeline');
         const assetPathFunction = assetPipelineService.assetPath;
-        console.log(assetPathFunction.apply(assetPipelineService, ['pixel.png']));
+        assert.equal('/assets/pixel.png', assetPathFunction.apply(assetPipelineService, ['pixel.png']));
         return done(null);
       });
     });
