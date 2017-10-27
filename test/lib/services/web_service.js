a661621b4f1fea0dd51a370e0f1b0f40849527c3
Gaurav Tiwari <gauravtiwari5050@gmail.com>
2017-10-27 23:22:44 +0530

    fix test app routes file

--- test/lib/services/web_service.js
+++ test/lib/services/web_service.js
@@ -2,7 +2,6 @@ const path = require('path');
 const request = require('supertest'); //eslint-disable-line
 const { Application } = require('../../../lib/application/application');
 
-
 describe('class: RouteService', () => {
   describe('method: initialize', () => {
     it('should correctly set asset pipeline methods', (done) => {
@@ -11,12 +10,13 @@ describe('class: RouteService', () => {
         env: 'test',
       });
 
+
       application.initializeServices(['asset_pipeline', 'view', 'route', 'web'], (err) => {
         if (err) {
           return done(err);
         }
         const webService = application.service('web');
-        webService.setupRoutes((errSetupRoutes) => {
+        return webService.setupRoutes((errSetupRoutes) => {
           if (errSetupRoutes) {
             return done(errSetupRoutes);
           }
@@ -24,7 +24,7 @@ describe('class: RouteService', () => {
           return request(webApplication)
             .get('/')
             .expect(200)
-            .end((errSuperTest, response) => {
+            .end((errSuperTest) => {
               done(errSuperTest);
             });
         });
--- test/test_apps/basic_app/config/routes.js
+++ test/test_apps/basic_app/config/routes.js
@@ -1,8 +1,11 @@
 const Rihand = require('../../../../index.js');
 
+const sampleConstraint = function (request) {
+  return false;
+};
 module.exports.loadRoutes = () => {
   Rihand.Route.getInstance().setup(function () {
-    this.Get('/ => landing#home');
+    this.Get('/ => landing#home').constraint(sampleConstraint);
     this.Get('/ => landing#home2');
     this.namespace('api').json().setup(function () {
       this.namespace('v1').setup(function () {
