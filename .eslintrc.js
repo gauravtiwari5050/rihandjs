5bc8921be799f64d8f0627289d49d62e06a55301
Gaurav Tiwari <gauravtiwari5050@gmail.com>
2018-05-27 21:27:59 +0530

    add support for cron jobs

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,6 +4,9 @@ module.exports = {
         "func-names": "off",
         "global-require": "off",
         "class-methods-use-this": "off",
+        "no-underscore-dangle": "off",
+        "no-restricted-syntax": "off",
+        "no-await-in-loop": "off",
         "import/no-extraneous-dependencies": ["error", {"devDependencies": false, "optionalDependencies": false, "peerDependencies": false}]
     },
     "env": {
